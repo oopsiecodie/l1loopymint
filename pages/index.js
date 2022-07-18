3dd42f8a8a5e282e28bc41629367386fa56afe4c
oopsiecodie <107747695+oopsiecodie@users.noreply.github.com>
2022-07-18 17:02:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        <h1 style="color:pink " >
           Layer1Loopys
-        </h1>
         </h1>
         
         <GetStarted />
